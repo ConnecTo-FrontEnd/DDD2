764015f5440972d26a7d8c83750e336c7ab69a0d
codeisneverodd <codeisneverodd@gmail.com>
2022-10-12 16:46:39 +0900

    feat: 테스트용 네비게이터 추가

--- App.js
+++ App.js
@@ -1,23 +1,17 @@
 // 루트 컴포넌트
 import Component from './core/Component.js';
 import { Loading, Setting, MainBeforeSignIn, MainAfterSignIn, SignIn, SignUp } from './pages/index.js';
-import { routes } from './router/index.js';
+import { navigator, routes } from './router/index.js';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      problems: [
-        { link: '/', level: 1, title: '사다리꼴 넓이' },
-        { link: '/', level: 3, title: 'CBD' },
-        { link: '/', level: 2, title: '프록시' },
-        { link: '/', level: 2, title: '화이트갈릭 싸이버거' },
-        { link: '/', level: 3, title: '이태리 부대찌개' },
-        { link: '/', level: 4, title: '부대전골' },
-        { link: '/', level: 5, title: '맘스터치' },
-      ],
-      isLogIn: true,
-    };
+
+    window.addEventListener('click', e => {
+      if (!e.target.matches('a')) return;
+      e.preventDefault();
+      navigator.go(e.target.pathname);
+    });
   }
 
   domStr() {
@@ -26,9 +20,14 @@ class App extends Component {
     const route = routes.find(({ path }) => path === currentPath);
     const Page = route?.page ?? SignIn;
     return `
-      ${new Page({
-        state: this.state,
-      }).render()}
+      <div>
+        <a href="/">Home </a>
+        <a href="/mainAfterSignIn">mainAfterSignIn</a>
+        ${new Page({
+          state: this.state,
+        }).render()}
+      </div>
+      
     `;
   }
 }
