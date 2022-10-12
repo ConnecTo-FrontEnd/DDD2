d4951bd288eefc6ff0e9bc3e77da8a32206b8997
codeisneverodd <codeisneverodd@gmail.com>
2022-10-12 16:48:11 +0900

    feat: setState 추가

--- core/Component.js
+++ core/Component.js
@@ -1,4 +1,5 @@
 import eventHolder from '../render/eventHolder.js';
+import render from '../render/render.js';
 
 class Component {
   constructor(props) {
@@ -8,13 +9,14 @@ class Component {
     this.holdEvents();
   }
 
-  classAdder(domStr, uuid) {
+  setState(newState) {
+    this.state = { ...this.state, ...newState };
+    render(this, document.querySelector(`[data-uuid='${this.uuid}']`));
+  }
+
+  uuidAdder(domStr, uuid) {
     const firstTag = /<.*>/;
-    const classRegex = /class( )*=( )*("|')(.)*('|")/;
-    return domStr.replace(firstTag, tag => {
-      if (!tag.match(classRegex)) return tag.split('>')[0] + ` class="${uuid}">`;
-      return tag.replace(classRegex, c => `class="${uuid} ${c.replaceAll("'", '"').split('"')[1]}"`);
-    });
+    return domStr.replace(firstTag, tag => tag.split('>')[0] + ` data-uuid="${uuid}">`);
   }
 
   converter(selector) {
@@ -29,23 +31,23 @@ class Component {
 
     for (const event of events) {
       if (event.selector === 'window' || event.selector === null) {
-        eventHolder.push(event);
+        eventHolder.push({ ...event, uuid: this.uuid });
         continue;
       }
       const { selector, handler } = event;
 
       event.handler = e => {
-        if (e.target.closest(`.${this.uuid}`) && e.target.closest(selector)) {
+        if (e.target.closest(`[data-uuid='${this.uuid}']`) && e.target.closest(selector)) {
           handler(e);
         }
       };
 
-      eventHolder.push(event);
+      eventHolder.push({ ...event, uuid: this.uuid });
     }
   }
 
   render() {
-    return this.classAdder(this.domStr(), this.uuid);
+    return this.uuidAdder(this.domStr(), this.uuid);
   }
 }
 
