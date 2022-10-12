d273bc1af3019430600971d7dbf7e0ea050532f1
codeisneverodd <codeisneverodd@gmail.com>
2022-10-12 16:47:54 +0900

    feat: navigator 추가

--- router/index.js
+++ router/index.js
@@ -1,8 +1,24 @@
-import {SignIn, MainAfterSignIn} from '../pages/index.js'
+import { SignIn, MainAfterSignIn } from '../pages/index.js';
+import render from '../render/render.js';
 
 const routes = [
-  {path:'/signin', page: SignIn},
-  {path:'/mainAfterSignIn', page: MainAfterSignIn}
-]
+  { path: '/signin', page: SignIn },
+  { path: '/mainAfterSignIn', page: MainAfterSignIn },
+];
 
-export {routes}
+const navigator = {
+  go(_path) {
+    const currentPath = window.location.pathname.replace('/index.html', '');
+    if (currentPath === _path) return;
+
+    const route = routes.find(({ path }) => path === _path);
+
+    window.history.pushState(null, null, route?.path ?? '/');
+    render();
+  },
+  back() {
+    render();
+  },
+};
+
+export { routes, navigator };
