f92a6da104a47c0daef5b3fee7cbc923acbb275e
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-10-12 21:18:48 +0900

    feat: 스타일 추가

---
+++ styles/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  // eslint-prettier-ignore
+  'orange-color': '#fb8d57',
+  'lightgray-color': '#a0a0a0',
+  'white-color': '#ffffff',
+  'black-color': '#1a2b3c',
+  'font-kr-bold': "bold 14px 'Noto Sans KR', sans-serif",
+  'font-kr-regular': "regular 14px 'Noto Sans KR', sans-serif",
+  'font-en-bold': "bold 14px 'Ubuntu', sans-serif",
+  'font-en-regular': "regular 14px 'Ubuntu', sans-serif",
+};
+
+export default theme;
