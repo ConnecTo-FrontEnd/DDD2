7a05d19ff6f031e993f9f2a9a15806784d449088
codeisneverodd <codeisneverodd@gmail.com>
2022-10-12 16:47:36 +0900

    feat: 테스트용 버튼 및 이벤트 핸들러 추가

--- pages/MainAfterSignIn.js
+++ pages/MainAfterSignIn.js
@@ -2,10 +2,45 @@ import Component from '../core/Component.js';
 import { Profile, ProblemList } from '../components/index.js';
 
 class MainAfterSignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      problems: [
+        { link: '/', level: 1, title: '사다리꼴 넓이' },
+        { link: '/', level: 3, title: 'CBD' },
+        { link: '/', level: 2, title: '프록시' },
+        { link: '/', level: 2, title: '화이트갈릭 싸이버거' },
+        { link: '/', level: 3, title: '이태리 부대찌개' },
+        { link: '/', level: 4, title: '부대전골' },
+        { link: '/', level: 5, title: '맘스터치' },
+      ],
+    };
+  }
+
+  addEventListener() {
+    return [
+      {
+        type: 'click',
+        selector: 'button',
+        handler: e => {
+          this.removeTop.call(this);
+        },
+      },
+    ];
+  }
+
+  removeTop() {
+    this.setState({ problems: this.state.problems.slice(1) });
+  }
+
   domStr() {
     return `
-    ${new Profile().render()}
-    ${new ProblemList(this.props).render()}`;
+      <div>
+        <button>맨위 지우기</button>
+        ${new Profile().render()}
+        ${new ProblemList({ ...this.props, state: this.state }).render()};
+      </div>
+    `;
   }
 }
 
