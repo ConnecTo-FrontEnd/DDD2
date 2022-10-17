bbab0c554fb0909ff02d2b555a1c7c8dae0fe26f
codeisneverodd <codeisneverodd@gmail.com>
2022-10-17 17:13:59 +0900

    feat: 404 페이지 구현 및 버튼 컴포넌트 props 추가

--- components/Button.js
+++ components/Button.js
@@ -1,18 +1,26 @@
 import Component from '../library/Component.js';
+import styled from '../library/styled.js';
 
-const styledButton = {
+const button = styled({
   margin: '0 auto',
   width: '70%',
   height: '3rem',
   border: '2px solid black',
-  'border-radius': '16px',
+  'border-radius': '10px',
   'font-size': '1rem',
   'background-color': '#f7f7f7',
-};
+});
 
 class Button extends Component {
+  // {style, text, onClick} = this.props
   domStr() {
-    return `<button style="${this.converter(styledButton)}">${this.props}</button>`;
+    const { style, text } = this.props;
+    return `<button style="${button}; ${style}">${text}</button>`;
+  }
+
+  addEventListener() {
+    const { onClick } = this.props;
+    return [{ type: 'click', selector: 'button', handler: onClick }];
   }
 }
 
--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,51 @@
+import Button from '../components/Button.js';
 import Component from '../library/Component.js';
+import styled from '../library/styled.js';
+import { router } from '../router/index.js';
+import theme from '../styles/theme.js';
+
+const container = styled({
+  display: 'flex',
+  'flex-direction': 'column',
+  'margin-top': '50%',
+  transform: 'translateY(30%)',
+  gap: '40px',
+  'align-items': 'center',
+  'justify-content': 'center',
+});
+const homeButton = styled({
+  'background-color': theme['orange-color'],
+  color: theme['white-color'],
+  border: 'none',
+});
+
+const NotFoundImage = styled({
+  background: 'url(../assets/404.png)',
+  'background-size': 'contain',
+  'background-repeat': 'no-repeat',
+  width: '200px',
+  height: '100px',
+});
+
+const Comment = styled({
+  color: theme['orange-color'],
+  font: theme['font-kr-regular'],
+  'font-size': '24px',
+});
 
 class NotFound extends Component {
   domStr() {
     return `
-    404`;
+      <div style="${container}">
+        <div style="${NotFoundImage}"/></div>
+        <p style="${Comment}">아무것도 없솔!</p>
+        ${new Button({ text: '홈으로', style: homeButton, onClick: this.goHome.bind(this) }).render()}
+      </div>
+    `;
+  }
+
+  goHome() {
+    router.go('/');
   }
 }
 
