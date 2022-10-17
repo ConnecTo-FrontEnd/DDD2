2be5d6a30103df22c2c3e0200a91fc8bcdf0344f
GuRaBang <qkrdbsgk00@gmail.com>
2022-10-17 13:47:19 +0900

    feat: UserSetting 컴포넌트와 Setting페이지 구현

--- pages/Setting.js
+++ pages/Setting.js
@@ -13,34 +13,3 @@ class Setting extends Component {
 }
 
 export default Setting;
-
-{
-  /* <header class="nav">
-      <button class="back">뒤로가기</button>
-    </header>
-    <h1>설정</h1>
-    <form action="" class="form-setting">
-      <div class="input-container">
-        <label for="siginin-userid">몇 문제씩</label>
-        <input type="text" id="siginin-userid" name="userid" required />
-      </div>
-      <div class="input-container">
-        <label for="setting-password">몇 일마다</label>
-        <input type="password" id="setting-password" name="password" required />
-      </div>
-      <h2>플랫폼</h2>
-      <div class="checkbox-container">
-        <input type="checkbox" id="setting-platform" name="programmers" />
-        <label for="setting-platform">프로그래머스</label>
-      </div>
-      <div class="checkbox-container">
-        <input type="checkbox" id="setting-platform" name="backjoon" />
-        <label for="setting-platform">백준</label>
-      </div>
-      <div class="checkbox-container">
-        <input type="checkbox" id="setting-platform" name="leetcode" />
-        <label for="setting-platform">leetcode</label>
-      </div>
-    </form>
-    <button class="button save-button" disabled>저장</button> */
-}
