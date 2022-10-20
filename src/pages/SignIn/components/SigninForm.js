96de45f4fd2f2a73987eb17e459c1989bcc26da7
GuRaBang <qkrdbsgk00@gmail.com>
2022-10-20 12:28:09 +0900

    style: 로그인, 회원가입 페이지에 팝업 메시지 추가 및 레이아웃 수정

--- src/pages/SignIn/components/SigninForm.js
+++ src/pages/SignIn/components/SigninForm.js
@@ -1,5 +1,5 @@
 import Component from '../../../library/Component.js';
-import { SchemeInput, StyledButton } from '../../../shared/components/index.js';
+import { SchemeInput, StyledButton, PopUpMsg } from '../../../shared/components/index.js';
 import { SigninScheme } from '../../../shared/scheme/scheme.js';
 import styled from '../../../library/styled.js';
 import { requestSignIn } from '../../../shared/store/userInfo.js';
@@ -7,13 +7,17 @@ import { router } from '../../../shared/router/index.js';
 import theme from '../../../shared/styles/theme.js';
 
 const styles = {
-  formContainer: styled({
+  form: styled({
+    margin: '32px auto 0px',
+    padding: '0 2rem',
     '@desktop': {
-      margin: '0 auto',
       width: '900px',
+      padding: '0 4rem',
     },
   }),
   btnCommon: styled({
+    width: '100%',
+    'margin-top': '1rem',
     'border-radius': '10px',
     font: theme['font-kr-bold'],
     'font-size': '16px',
@@ -33,33 +37,39 @@ const styles = {
     color: theme['orange-color'],
     'font-size': '14px',
   }),
+  popUpMsg: styled({
+    'background-color': theme['lightgray-color'],
+  }),
 };
 
 class SigninForm extends Component {
   constructor() {
     super();
     this.signinScheme = new SigninScheme();
-    this.state = { userid: '', password: '', errorMsg: '' };
+    this.state = { userid: '', password: '', errorMsg: '', isPopUp: false };
   }
 
   domStr() {
-    const { userid, password, errorMsg } = this.state;
+    const { userid, password, errorMsg, isPopUp } = this.state;
     this.signinScheme.userid.value = userid;
     this.signinScheme.password.value = password;
     const { isValid } = this.signinScheme;
 
     return `
-      <form ${styles.formContainer} class="signin-form">
-        ${Object.values(this.signinScheme)
-          .map(scheme => new SchemeInput({ scheme, onInput: this.onInput.bind(this) }).render())
-          .join('')}
-        <div ${styles.errorMsg}>${errorMsg ?? ''}</div>  
-        ${new StyledButton({
-          style: styles.submitBtn[isValid ? 'active' : 'disabled'] + styles.btnCommon,
-          text: 'Sign in',
-          disabled: !isValid,
-        }).render()}
-      </form>`;
+      <div>
+        <form ${styles.form} class="signin-form">
+          ${Object.values(this.signinScheme)
+            .map(scheme => new SchemeInput({ scheme, onInput: this.onInput.bind(this) }).render())
+            .join('')}
+          ${new StyledButton({
+            style: styles.submitBtn[isValid ? 'active' : 'disabled'] + styles.btnCommon,
+            text: 'Sign in',
+            disabled: !isValid,
+          }).render()}
+          ${new PopUpMsg({ msg: errorMsg, isPopUp, style: styles.popUpMsg }).render()}
+        </form>
+      </div>
+      `;
   }
 
   onInput(e) {
@@ -81,7 +91,10 @@ class SigninForm extends Component {
           });
           if (res.ok) return router.go('/');
 
-          this.setState.call(this, { userid: '', password: '', errorMsg: res.err.response.data.error });
+          this.setState.call(this, { errorMsg: res.err.response.data.error, isPopUp: true });
+          setTimeout(() => {
+            this.setState.call(this, { isPopUp: false });
+          }, 1200);
         },
       },
     ];
